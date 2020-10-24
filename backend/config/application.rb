4a1cadb00302da1fc77cb2ed707a8be0853ed64b
medalhkr <medalhkr@gmail.com>
2020-10-24 11:40:06 +0900

    application config

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,35 +1,32 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.cache_classes = true
+    config.eager_load = true
+    config.consider_all_requests_local = false
+    config.action_controller.perform_caching = true
+
+    config.log_level = :info
+    config.log_tags = [ :request_id ]
+    config.logger = ActiveSupport::Logger.new(STDOUT)
+    config.log_formatter = ::Logger::Formatter.new
+    config.action_view.logger = nil
+
+    config.public_file_server.enabled = false
+    config.i18n.fallbacks = true
+    config.active_record.dump_schema_after_migration = false
   end
 end
