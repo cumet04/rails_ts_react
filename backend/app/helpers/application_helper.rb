3f944ec6534f6ca3412457983db6a33de428266b
medalhkr <medalhkr@gmail.com>
2020-10-25 22:04:22 +0900

    やれるところまではやった

--- backend/app/helpers/application_helper.rb
+++ backend/app/helpers/application_helper.rb
@@ -5,6 +5,15 @@ module ApplicationHelper
   end
 
   def prop_data_json
-    (@_view_props || {}).to_json
+    json = @_view_props || {}
+    validate_json(json) if Rails.env.development? || Rails.env.test?
+    json.to_json
+  end
+
+  def validate_json(json)
+    # ruby-json-schemaが古くてtypescript-json-schemaが吐くJSON Schemaのバージョンと合わなかった。終了。
+    name = view_uri.delete_prefix("/").tr("/", "_")
+    schema = "#{Rails.root}/lib/schema/PagePropsType.#{name}.json"
+    JSON::Validator.validate!(schema, json)
   end
 end
