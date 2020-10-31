bfd18b156a98ae1d833ffc43cb503cbdbceb28a8
medalhkr <medalhkr@gmail.com>
2020-11-01 00:32:33 +0900

    backend api test

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -10,4 +10,12 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
   config.action_controller.allow_forgery_protection = false
   config.active_support.deprecation = :stderr
+
+  committee_params = {
+    schema_path: Rails.root.join("../api/openapi.yml").to_s,
+    prefix: "/api",
+    raise: true
+  }
+  config.middleware.use(Committee::Middleware::RequestValidation, committee_params)
+  config.middleware.use(Committee::Middleware::ResponseValidation, committee_params)
 end
